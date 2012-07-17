65770df95c1c0ca65d9178fed8ea9dee8a9bd950
Alberto Mardegan <alberto.mardegan@canonical.com>
2012-07-17 16:09:22 +0300

    Indentation, copyright, gitignore

--- tests/oauth2plugintest.h
+++ tests/oauth2plugintest.h
@@ -2,8 +2,9 @@
  * This file is part of oauth2 plugin
  *
  * Copyright (C) 2010 Nokia Corporation.
+ * Copyright (C) 2012 Canonical Ltd.
  *
- * Contact: Alberto Mardegan <alberto.mardegan@nokia.com>
+ * Contact: Alberto Mardegan <alberto.mardegan@canonical.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
